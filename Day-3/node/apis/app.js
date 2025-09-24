d8da69cdba8093df76b6e1e71910d1c5a46f9e95
gitzzy <tyagidevansh10@gmail.com>
2025-09-24 21:14:26 +0530

    get request

--- Day-3/node/apis/app.js
+++ Day-3/node/apis/app.js
@@ -3,8 +3,27 @@ const data = require('./MOCK_DATA.json')
 const app = exp()
 const port = 3000
 
-app.get('/users',(req,res) => {
+app.get('/' , (req,res) => {
+    res.send('Working')
+})
+
+app.get('/api/users',(req,res) => {
     return res.json(data)
+})
+
+app.get('/users',(req,res) => {
+    const html = `
+    <ul>
+        ${data.map(user => `<li>${user.first_name}</li>`).join('')}
+    </ul>
+    `
+    return res.send(html)
+})
+
+app.get('/api/users/:id' ,(req,res) => {
+    let id = Number(req.params.id)
+    const user = data.find((user) => user.id === id)
+    return res.json(user)
 })
 
 app.listen(port, () => {
