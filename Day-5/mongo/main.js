1a10dd18a722f729bab27dd2b1df0b9b0d30bc83
gitzzy <tyagidevansh10@gmail.com>
2025-09-29 12:57:28 +0530

    mongo crud

--- Day-5/mongo/main.js
+++ Day-5/mongo/main.js
@@ -14,17 +14,36 @@ let con = await mongoose.connect(mongoString)
     .catch((err) => console.log(err))
 
 app.get('/',async (req,res) => {
-
     //Adding Mongo data
     const user = new userModel({
-        name:'Test1',
-        username:'test',
+        name:'Test',
+        username:'test1',
         password:'testpass'
     })
     await user.save()
     res.send('<h1>MongoDb connection</h1>')
 })
 
+//Fetching data
+app.get('/users' ,async (req,res) => {
+    const user = await userModel.find()
+    res.json(user)
+})
+
+//Find by id
+app.get('/users/:id' ,async(req,res) => {
+    const user  = await userModel.findById(req.params.id)
+    res.json(user)
+})
+
+//Update user
+app.get('/update' , async(req,res) => {
+  let update = await userModel.findOneAndUpdate({username:'test2'},
+    {$set:{name:'Devansh'}},
+    {new:true}
+   )
+   res.json(update)
+})
 
 
 app.listen(port,() => {
